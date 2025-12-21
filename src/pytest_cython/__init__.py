b93c46a97ea5bde7e0685587ccca56c678933d13
Leo Singer <leo.p.singer@nasa.gov>
2025-12-21 12:27:14 -0500

    Remove dependency on pkg_resources (#72)

--- src/pytest_cython/__init__.py
+++ src/pytest_cython/__init__.py
@@ -1,9 +1,8 @@
+from importlib.metadata import version, PackageNotFoundError
+
 try:
-    from pkg_resources import get_distribution
-    __version__ = get_distribution('pytest-cython').version
-    del get_distribution
-except Exception:
+    __version__ = version("pytest-cython")
+except PackageNotFoundError:
     import warnings
-    warnings.warn('could not get pytest-cython version; pkg_resources '
-                  'not available or package not installed')
+    warnings.warn('could not get pytest-cython version')
     __version__ = '0.0.0'
